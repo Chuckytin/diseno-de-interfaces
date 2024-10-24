4e1684ed770397098ce27fe12faf4b010166af8d
Chuckytin <angeltorcalcatalan@gmail.com>
2024-10-24 16:28:54 +0200

    actualizando comentario del ejercicio

--- PracticaWindowsForms/PracticaWindowsForms/Program.cs
+++ PracticaWindowsForms/PracticaWindowsForms/Program.cs
@@ -1,4 +1,9 @@
 namespace PracticaWindowsForms
+    /*
+     * Ejercicio de adivinar el número.
+     * 3 intentos para adivinarlo.
+     * No se termina el juego hasta acertar el número y se cierra.
+     */
 {
     internal static class Program
     {
