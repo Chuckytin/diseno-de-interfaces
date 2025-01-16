a816b10b4e86fcab5e7edffdec44c2073cb29694
Chuckytin <angeltorcalcatalan@gmail.com>
2025-01-16 17:46:36 +0100

    practicas con WPF

---
+++ CC_WPF_Tiers_Ex1/CC_WPF_Tiers_Ex1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CC_WPF_Tiers_Ex1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ CC_WPF_Tiers_Ex1/CC_WPF_Tiers_Ex1/Componentes/Casilla.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CC_WPF_Tiers_Ex1.Componentes
+{
+    /// <summary>
+    /// Lógica de interacción para Casilla.xaml
+    /// </summary>
+    public partial class Casilla : UserControl
+    {
+
+        public static readonly DependencyProperty dependencyProperty = 
+            DependencyProperty.Register("Description", typeof(string), typeof(Casilla),
+            new PropertyMetadata(string.Empty));
+
+        public string Description { get; set; }
+
+        public Casilla()
+        {
+            InitializeComponent();
+        }
+    }
+}
